32cfc7b07136cabf61dadba1fdbc6d7ff73ead24
Natalia_Sherchenkova <tataterer@gmail.com>
2021-12-17 16:05:36 +0000

    Finished TD forms

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -10,16 +10,51 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = "pet";
   answer = '';
+  genders = ['male', 'female'];
+  defaultGender = "female";
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submittedForm = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // Option 1
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'female'
+    // })
+
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
   //   console.log(form)
   // }
 
+  
   onSubmit(){
-    console.log(this.signupForm)
+    this.submittedForm = true
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    
+    this.signupForm.reset()
+    // or can pas an objects with set values to reset
   }
 }
